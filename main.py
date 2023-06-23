28013d29beaee5a4ad8a8016fea07f1af48943f7
Adriano Hernandez <adrianohernandez2000@gmail.com>
2023-06-23 09:14:46 -0700

    Matrix multiplication, broadcast addition, etc... seems to work

--- main.py
+++ main.py
@@ -35,6 +35,13 @@ class Matrix:
         assert len(self.values) == height
         assert all([len(self.values[i]) == width for i in range(height)])
     
+    def __eq__(self, __value: object) -> bool:
+        return type(__value) == Matrix and (
+            self.height == __value.height and self.width == __value.width) and (
+            all([
+                self.values[i][j] == __value.values[i][j] for i in range(self.height) for j in range(self.width)])
+            )
+    
     def lmatmul(self, other: Matrix) -> Matrix:
         # Our matrix is left
         return Matrix.matmul(self, other)
@@ -65,7 +72,7 @@ class Matrix:
                     left_row = lambda j: left.values[orow][j]
                     right_col = lambda i: right.values[i][ocol]
                     out[orow][ocol] += left_row(dentry) * right_col(dentry)
-        return out
+        return Matrix(left.height, right.width, values=out)
     
     @staticmethod
     def add(left: Matrix, right: Matrix) -> Matrix:
@@ -117,7 +124,7 @@ class Matrix:
         for i in range(height):
             for j in range(width):
                 out[i][j] = pop(left.values[i][j], get_right_value(i, j))
-        return out
+        return Matrix(height, width, values=out)
 
 
 class CGNodeState(Enum):
@@ -159,167 +166,191 @@ class CGNodeState(Enum):
 #     def MSE():
 #         pass
 
-class LossCG:
-    def __init__(self, l: List[Tuple[Function, Optional[Parameter]]]) -> None:
-        self.funcs = []
-        self.params = []
-        for func, param in l:
-            self.funcs.append(func)
-            if param is not None:
-                self.params.append(param)
-                self.funcs[-1].parameter = param
-        for i in range(len(self.funcs) -1):
-            self.funcs[i].next = self.funcs[i+1]
-            self.funcs[i+1].prev = self.funcs[i]
-    def forward(self, input: Matrix) -> Matrix:
-        return self.funcs[0].forward(input)
-    def backward(self, loss: Matrix) -> None:
-        self.funcs[-1].backward(loss)
-    def step(self) -> None:
-        for param in self.params:
-            param.step()
+# class LossCG:
+#     def __init__(self, l: List[Tuple[Function, Optional[Parameter]]]) -> None:
+#         self.funcs = []
+#         self.params = []
+#         for func, param in l:
+#             self.funcs.append(func)
+#             if param is not None:
+#                 self.params.append(param)
+#                 self.funcs[-1].parameter = param
+#         for i in range(len(self.funcs) -1):
+#             self.funcs[i].next = self.funcs[i+1]
+#             self.funcs[i+1].prev = self.funcs[i]
+#     def forward(self, input: Matrix) -> Matrix:
+#         return self.funcs[0].forward(input)
+#     def backward(self, loss: Matrix) -> None:
+#         self.funcs[-1].backward(loss)
+#     def step(self) -> None:
+#         for param in self.params:
+#             param.step()
     
-    def clear(self) -> None:
-        for func in self.funcs:
-            func.state = CGNodeState.EMPTY
-            func.derivative_wrt_loss = None
-        for param in self.params:
-            param.state = CGNodeState.EMPTY
-            param.derivative_wrt_loss = None
+#     def clear(self) -> None:
+#         for func in self.funcs:
+#             func.state = CGNodeState.EMPTY
+#             func.derivative_wrt_loss = None
+#         for param in self.params:
+#             param.state = CGNodeState.EMPTY
+#             param.derivative_wrt_loss = None
 
-class CGNode(ABC):
-    def __init__(self) -> None:
-        self.state = CGNodeState.EMPTY
-        self.id = uuid4()
-        self.derivative_wrt_loss: Matrix = None
-        self.output_value: Callable[[], Matrix] = lambda: None
+# class CGNode(ABC):
+#     def __init__(self) -> None:
+#         self.state = CGNodeState.EMPTY
+#         self.id = uuid4()
+#         self.derivative_wrt_loss: Matrix = None
+#         self.output_value: Callable[[], Matrix] = lambda: None
 
-    def uid(self) -> int:
-        return self.id
-    def backward(self, next_derivative: Any = None) -> Any:
-        raise NotImplementedError
-    def step(self) -> None:
-        raise NotImplementedError
+#     def uid(self) -> int:
+#         return self.id
+#     def backward(self, next_derivative: Any = None) -> Any:
+#         raise NotImplementedError
+#     def step(self) -> None:
+#         raise NotImplementedError
 
-class Parameter(CGNode):
-    def __init__(self, function: Optional[Function] = None) -> None:
-        super().__init__()
-        # self.state, self.id, self.derivative_wrt_loss
-        # (already present)
-        self.lr = 0.01
-        self.function = function
+# class Parameter(CGNode):
+#     def __init__(self, function: Optional[Function] = None) -> None:
+#         super().__init__()
+#         # self.state, self.id, self.derivative_wrt_loss
+#         # (already present)
+#         self.lr = 0.01
+#         self.function = function
     
-    def backward(self, derivative: Matrix = None) -> Any:
-        self.derivative_wrt_loss = derivative
-    def step(self) -> None:
-        output_value = self.output_value()
+#     def backward(self, derivative: Matrix = None) -> Any:
+#         self.derivative_wrt_loss = derivative
+#     def step(self) -> None:
+#         output_value = self.output_value()
 
-        # They must be matries
-        assert type(output_value) == list and len(output_value) >= 1
-        assert all([type(output_value[i]) == list and len(output_value[i]) >= 1 for i in range(len(output_value))])
+#         # They must be matries
+#         assert type(output_value) == list and len(output_value) >= 1
+#         assert all([type(output_value[i]) == list and len(output_value[i]) >= 1 for i in range(len(output_value))])
 
-        # The matrix shaps must match
-        assert len(self.derivative_wrt_loss) == len(output_value)
-        assert all(len(self.derivative_wrt_loss[i]) == len(output_value[i]) for i in range(len(self.derivative_wrt_loss)))
+#         # The matrix shaps must match
+#         assert len(self.derivative_wrt_loss) == len(output_value)
+#         assert all(len(self.derivative_wrt_loss[i]) == len(output_value[i]) for i in range(len(self.derivative_wrt_loss)))
         
-        # One for one step
-        for i in range(len(self.derivative_wrt_loss)):
-            for j in range(len(self.derivative_wrt_loss[i])):
-                output_value[i][j] -= self.lr * self.derivative_wrt_loss[i][j]
+#         # One for one step
+#         for i in range(len(self.derivative_wrt_loss)):
+#             for j in range(len(self.derivative_wrt_loss[i])):
+#                 output_value[i][j] -= self.lr * self.derivative_wrt_loss[i][j]
 
-class Function(Node):
-    def __init__(self, parameter: Optional[Parameter] = None, next: Optional[Function] = None, prev: Optional[Function] = None) -> None:
-        super().__init__()
-        # self.state, self.id, self.derivative_wrt_loss
-        # (already present)
-        self.parameter = parameter
-        self.next = next
-        self.prev = prev
+# class Function(CGNode):
+#     def __init__(self, parameter: Optional[Parameter] = None, next: Optional[Function] = None, prev: Optional[Function] = None) -> None:
+#         super().__init__()
+#         # self.state, self.id, self.derivative_wrt_loss
+#         # (already present)
+#         self.parameter = parameter
+#         self.next = next
+#         self.prev = prev
 
-    # Push forward the computation
-    def _calc_forward(self, input: Matrix, param_value: Optional[Matrix] = None) -> Matrix:
-        raise NotImplementedError
-    def forward(self, input: Matrix) -> Matrix:
-        assert self.state == NodeState.EMPTY
+#     # Push forward the computation
+#     def _calc_forward(self, input: Matrix, param_value: Optional[Matrix] = None) -> Matrix:
+#         raise NotImplementedError
+#     def forward(self, input: Matrix) -> Matrix:
+#         assert self.state == CGNodeState.EMPTY
 
-        outgoing = self._calc_forward(input, self.parameter.output_value() if self.parameter else None)
-        self.state = NodeState.FORWARD_FLOWED
+#         outgoing = self._calc_forward(input, self.parameter.output_value() if self.parameter else None)
+#         self.state = CGNodeState.FORWARD_FLOWED
 
-        if self.next is None:
-            return outgoing
-        return self.next.forward(outgoing)
+#         if self.next is None:
+#             return outgoing
+#         return self.next.forward(outgoing)
     
-    # Calculate the derivative w.r.t. the loss for the parameter and for the input
-    def _calc_derivative_param(self, next_derivative: Matrix) -> Matrix:
-        raise NotImplementedError
-    def _calc_derivative_input(self, next_derivative: Matrix) -> Matrix:
-        raise NotImplementedError
-    def backward(self, next_derivative: Any = None) -> None:
-        assert self.state == NodeState.FORWARD_FLOWED
-        assert (next_derivative is None) == (self.next is None)
+#     # Calculate the derivative w.r.t. the loss for the parameter and for the input
+#     def _calc_derivative_param(self, next_derivative: Matrix) -> Matrix:
+#         raise NotImplementedError
+#     def _calc_derivative_input(self, next_derivative: Matrix) -> Matrix:
+#         raise NotImplementedError
+#     def backward(self, next_derivative: Any = None) -> None:
+#         assert self.state == CGNodeState.FORWARD_FLOWED
+#         assert (next_derivative is None) == (self.next is None)
 
-        next_derivative = [[1.0]] if next_derivative is None else next_derivative
-        derivative_wrt_loss_input = self._calc_derivative_input(next_derivative)
-        derivative_wrt_loss_param = self._calc_derivative_param(next_derivative)
-        self.derivative_wrt_loss = derivative_wrt_loss_input
+#         next_derivative = [[1.0]] if next_derivative is None else next_derivative
+#         derivative_wrt_loss_input = self._calc_derivative_input(next_derivative)
+#         derivative_wrt_loss_param = self._calc_derivative_param(next_derivative)
+#         self.derivative_wrt_loss = derivative_wrt_loss_input
 
-        if self.parameter is not None:
-            # This just stores that derivative
-            self.parameter.backward(derivative_wrt_loss_param)
-        if self.prev is not None:
-            self.prev.backward(self.derivative_wrt_loss)
+#         if self.parameter is not None:
+#             # This just stores that derivative
+#             self.parameter.backward(derivative_wrt_loss_param)
+#         if self.prev is not None:
+#             self.prev.backward(self.derivative_wrt_loss)
 
-    # NOTE: does not implement step
-    def step(self) -> None:
-        raise ValueError("You tried to call step on a function?!")
+#     # NOTE: does not implement step
+#     def step(self) -> None:
+#         raise ValueError("You tried to call step on a function?!")
 
-class MatrixAffine(Parameter):
-    # NOTE that you must always multiple with input on the right so that
-    # we are interperting the columns as the vectors of the vector space
-    def __init__(self, height: int, width: int) -> None:
-        super().__init__()
-        self.matrix = [[random() for _ in range(width)] for _ in range(height)]
-        self.affine = [random() for _ in range(height)]
-        # List of rows, turn the affine into a row
-        self.output_value = lambda: self.matrix + [self.affine]
+# class MatrixAffine(Parameter):
+#     # NOTE that you must always multiple with input on the right so that
+#     # we are interperting the columns as the vectors of the vector space
+#     def __init__(self, height: int, width: int) -> None:
+#         super().__init__()
+#         self.matrix = [[random() for _ in range(width)] for _ in range(height)]
+#         self.affine = [random() for _ in range(height)]
+#         # List of rows, turn the affine into a row
+#         self.output_value = lambda: self.matrix + [self.affine]
     
-class MatrixAffineMultiply(Function):
-    def __init__(self, *args, **kwargs) -> None:
-        super().__init__(*args, **kwargs)
-    def _calc_forward(self, input: Matrix, param_value: Optional[Matrix] = None) -> Matrix:
-        # Must have a matrix and a vector (affine) parameter
-        assert param_value
-        assert len(param_value) >= 2
+# class MatrixAffineMultiply(Function):
+#     def __init__(self, *args, **kwargs) -> None:
+#         super().__init__(*args, **kwargs)
+#     def _calc_forward(self, input: Matrix, param_value: Optional[Matrix] = None) -> Matrix:
+#         # Must have a matrix and a vector (affine) parameter
+#         assert param_value
+#         assert len(param_value) >= 2
 
-        matrix = param_value[:-1]
-        affine_row = param_value[-1:]
-        assert len(affine_row) == 1
+#         matrix = param_value[:-1]
+#         affine_row = param_value[-1:]
+#         assert len(affine_row) == 1
         
-        # Turn it into an affine column
-        affine = [[affine_row[0][i]] for i in range(len(affine_row[0]))]
+#         # Turn it into an affine column
+#         affine = [[affine_row[0][i]] for i in range(len(affine_row[0]))]
 
-        # Adding enables broadcasting
-        mmed =  Matrix.matmul(matrix, input)
-        aed = Matrix.add(affine, mmed)
-        out = aed
+#         # Adding enables broadcasting
+#         mmed =  Matrix.matmul(matrix, input)
+#         aed = Matrix.add(affine, mmed)
+#         out = aed
 
-        return out
+#         return out
 
 if __name__ == "__main__":
-    matmul_seq = [
-        # Height, width for each
-        # Cancelled: 1st reduce from 28x28 to 4x28
-        # (MatrixAffineMultiply(), MatrixAffine(4, 28)),
-        # 2nd reduce from 4x28 to 1x28
-        (MatrixAffineMultiply(), MatrixAffine(1, 28)),
-    ]
-    # TODO not having transposes (or generally left vs. right mult.) is a real problem
+    """ Begin by testing our matrix code """
 
-    matmul_graph = LossGraph(matmul_seq)
-    input = [[random() for _ in range(28)] for _ in range(28)]
-    print("---image---")
-    print(input)
-    print("-----------")
+    mat1 = Matrix(2, 3, values=[[1, 2, 3], [4, 5, 6]])
+    mat2 = Matrix(3, 2, values=[[1, 0], [1, 0], [1, 0]])
+    mat1a = Matrix(2, 3, values=[[1, 1, 1], [1, 1, 1]])
+    matb = Matrix(1, 3, values=[[1, 1, 1]])
 
-    output = matmul_graph.forward(input)
-    print(output)
+    # Check that matmul works
+    assert Matrix.matmul(mat1, mat2).values == [[6, 0], [15, 0]]
+    assert Matrix.matmul(mat2, mat1).values == [[1, 2, 3], [1, 2, 3], [1, 2, 3]]
+    assert Matrix.matmul(mat1, mat2) == mat1.lmatmul(mat2) and mat1.lmatmul(mat2) == mat2.rmatmul(mat1)
+    assert Matrix.matmul(mat2, mat1) == mat2.lmatmul(mat1) and mat1.lmatmul(mat2) == mat2.rmatmul(mat1)
+    # Check that transpose works
+    assert matb.transpose().values == [[1], [1], [1]]
+    # Check that addition works
+    assert Matrix.add(mat1, mat1a).values == [[2, 3, 4], [5, 6, 7]]
+    # Check that pointwise multiplication works
+    assert Matrix.pmult(mat1, mat1a) == mat1
+    # Check that broadcasting properly works
+    assert Matrix.add(mat1, matb).values == [[2, 3, 4], [5, 6, 7]] and Matrix.add(matb, mat1) == Matrix.add(mat1, matb)
+    assert Matrix.pmult(mat1, matb).values == [[1, 2, 3], [4, 5, 6]] and Matrix.pmult(matb, mat1) == Matrix.pmult(mat1, matb)
+    assert Matrix.add(mat2, matb.transpose()).values == [[2, 1], [2, 1], [2, 1]] and Matrix.add(matb.transpose(), mat2) == Matrix.add(mat2, matb.transpose())
+    assert Matrix.pmult(mat2, matb.transpose()).values == [[1, 0], [1, 0], [1, 0]] and Matrix.pmult(matb.transpose(), mat2) == Matrix.pmult(mat2, matb.transpose())
+
+    # matmul_seq = [
+    #     # Height, width for each
+    #     # Cancelled: 1st reduce from 28x28 to 4x28
+    #     # (MatrixAffineMultiply(), MatrixAffine(4, 28)),
+    #     # 2nd reduce from 4x28 to 1x28
+    #     (MatrixAffineMultiply(), MatrixAffine(1, 28)),
+    # ]
+    # # TODO not having transposes (or generally left vs. right mult.) is a real problem
+
+    # matmul_graph = LossGraph(matmul_seq)
+    # input = [[random() for _ in range(28)] for _ in range(28)]
+    # print("---image---")
+    # print(input)
+    # print("-----------")
+
+    # output = matmul_graph.forward(input)
+    # print(output)
